e6af104d15d0e332cfea422e2209a6e092296f13
Luquebiqueno <dikizekopires@gmail.com>
2021-05-25 11:39:47 -0300

    Inclusão dos scripts sql

---
+++ AppDapper.Repository/SqlScripts/Database/CreateDatabase.sql
@@ -0,0 +1,14 @@
+﻿Use master
+Go
+
+If Not Exists (Select * From sys.databases Where Name = 'AppDapper')
+Begin
+	Create Database AppDapper
+End
+Go
+
+If Exists (Select * From sys.databases Where Name = 'AppDapper')
+Begin
+	Use AppDapper
+End
+Go
---
+++ AppDapper.Repository/SqlScripts/Tables/Script_Schema.sql
@@ -0,0 +1,13 @@
+﻿Use AppDapper
+Go
+
+If Object_Id('Usuario') Is Null 
+Begin
+	Create Table Usuario 
+	(
+		 Id		Int Identity(1,1) Constraint PK_Usuario_Id Primary Key
+		,Nome	Varchar(100)	Not Null
+		,Ativo	Bit				Not Null
+	)
+End
+Go
